e53c56dc43e6104fd5be8b48fd118751b24e11c2
HaydenLe <haydennhanle@gmail.com>
2021-02-19 16:00:40 +0000

    adding circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,22 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
+            var circle = 0;
+            var circles = [];
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
-
+        var loopsCompleted = 0; 
+            while (loopsCompleted < 100) {
+                drawCircle();
+                    loopsCompleted++
+}
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -39,14 +47,15 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
-            
+         
             // TODO 5 : Call game.checkCirclePosition() on your circles.
-           
-
-            // TODO 8 : Iterate over the array
-           
+                       // TODO 8 : Iterate over the array
+           for (var i = 0; i < circles.length; i++) {
+            var eachValue = circles[i];
             
+            physikz.updatePosition(eachValue)
+            game.checkCirclePosition(eachValue) 
+           } 
         }
     
         /* 
@@ -60,11 +69,11 @@ var init = function (window) {
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
-            
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
+            if ( circle.y > canvas.length) { 
+                circle.y = 0;
+            } 
             
-
-
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
@@ -89,4 +98,4 @@ if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
-}
+    }
