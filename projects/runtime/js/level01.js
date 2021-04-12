40679c9609ef30f5af2613f0ba6d10f4dc172813
HaydenLe <haydennhanle@gmail.com>
2021-04-12 14:20:02 +0000

    add runtime project

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -19,17 +19,129 @@ var level01 = function (window) {
                 { "type": "sawblade", "x": 400, "y": groundY },
                 { "type": "sawblade", "x": 600, "y": groundY },
                 { "type": "sawblade", "x": 900, "y": groundY },
-            ]
-        };
+                { "type": "bullet", "x": 1000, "y": groundY }, 
+                { "type": "bullet", "x": 1200, "y": groundY }, 
+                { "type": "bullet", "x": 1300, "y": groundY }, 
+                { "type": "enemy", "x": 1000, "y": groundY - 50},
+                { "type": "reward", "x": 2000, "y": groundY - 60}, 
+            ] 
+        };  
+
+            for (var i = 0; i < levelData.gameItems.length; i++) {
+                 obj = levelData.gameItems[i]; 
+                 objX = obj.x; 
+                 objY = obj.y; 
+                 objType = obj.type; 
+
+                if (objType === "sawblade") { 
+                    createSawBlade(objX, objY);
+                } 
+                else if (objType === "bullet") { 
+                    createBullet(objX, objY);
+                }    
+                 else if (objType === "enemy") {
+                    createEnemy(objX, objY);
+                }                  
+                 else (objType === "reward") 
+                    createReward(objX, objY);
+            } 
+
         window.levelData = levelData;
         // set this to true or false depending on if you want to see hitzones
-        game.setDebugMode(true);
+        game.setDebugMode(false);
 
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
+          
+        function createSawBlade(x, y){
+        var hitZoneSize = 25;
+        var damageFromObstacle = 10;
+        var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle); 
+        sawBladeHitZone.x = x;
+        sawBladeHitZone.y = y;  
 
+        game.addGameItem(sawBladeHitZone);  
+
+        var obstacleImage = draw.bitmap('img/sawblade.png');  
+        obstacleImage.x = -25; 
+        obstacleImage.y = -25;
+       sawBladeHitZone.addChild(obstacleImage); 
         
-        
+    }
+     //  createSawBlade(400, 220); 
+       //createSawBlade(850, 220); 
+      // createSawBlade(650, 340);  
+
+      function createBullet(x, y){  
+          var hitZoneSize = .04;
+        var damageFromObstacle = 44;
+        var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle); 
+        sawBladeHitZone.x = x;
+        sawBladeHitZone.y = y;  
+
+        game.addGameItem(sawBladeHitZone);  
+
+        var obstacleImage = draw.bitmap('img/bullet4 (1).png');  
+        obstacleImage.x = -.04; 
+        obstacleImage.y = -.04; 
+        obstacleImage.scaleX = .04; 
+        obstacleImage.scaleY = .04;  
+       sawBladeHitZone.addChild(obstacleImage);  
+      }
+
+      //createBullet(2444,244);
+      
+
+        function createEnemy(x,y) { 
+        var enemy = game.createGameItem('enemy',25);
+        var redSquare = draw.bitmap('img/bullet4 (1).png');  
+        redSquare.x = -.04;
+        redSquare.y = -.04; 
+        redSquare.scaleX = .04;
+        redSquare.scaleY = .04;
+        enemy.addChild(redSquare);  
+        enemy.x = x;
+        enemy.y = y; 
+        game.addGameItem(enemy); 
+        enemy.velocityX = -14; 
+        //enemy.rotationalVelocity = 10; 
+        enemy.onPlayerCollision = function() {
+            console.log('The bullets has hit Halle'); 
+            game.onPlayerCollision = game.changeIntegrity(-44);   
+            game.increaseScore(100);
+            enemy.fadeOut();
+        };  
+             
+        enemy.onProjectileCollision = function() { 
+            console.log("Halle has hit the bullets...somehow"); 
+            shrink()
+        };
+             };
+
+       //createBullets(1400,groundY-10);
+       //createBullets(800,groundY-100);
+       //createBullets(1200,groundY-50);
+
+        function createReward(x,y) { 
+            var reward = game.createGameItem('reward',25);
+            var greenSquare = draw.rect(25,25,'green');     
+            greenSquare.x = -25;
+            greenSquare.y = -25; 
+            reward.addChild(greenSquare);  
+            reward.x = x;
+            reward.y = y;   
+            game.addGameItem(reward);  
+            reward.velocityX = -5; 
+            reward.rotationalVelocity = 64; 
+            reward.onPlayerCollision = function() {
+             console.log('Halle got a reward'); 
+             game.onPlayerCollision = game.changeIntegrity(10);  
+             game.increaseScore(50);
+             reward.shrink(); 
+            };
+        };
+         //createReward(2200,groundY-50);
+
         
         // DO NOT EDIT CODE BELOW HERE
     }
--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -26,8 +26,8 @@ var background = function (window) {
         var background;
         
         // ANIMATION VARIABLES HERE:
-        
-     
+        var tree;
+        var buildings = [];
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -36,19 +36,45 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
-            background.addChild(backgroundFill);
-            
+            var backgroundFill = draw.rect(canvasWidth + 5, groundY +13 ,'#8FFEFF'); 
+            background.addChild(backgroundFill); 
+
+          
             // TODO: 3 - Add a moon and starfield
-            
-            
+
+          //var moon = draw.bitmap('img/moon.png');
+            //moon.x = 850;
+            //moon.y = 30;
+            //moon.scaleX = 0.5;
+            //moon.scaleY = 0.5;
+            //background.addChild(moon);
+
+            //var circle;  
+            //for(var i = 0; i < 100; i++) {
+                //var circle = draw.circle(10,'yellow','orange',2);
+                //circle.x = canvasWidth*Math.random();
+                //circle.y = groundY*Math.random();
+                //background.addChild(circle); 
+           // } 
+
+
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
-            
+            for(var i=0;i<5;++i) {
+            var buildingHeight = Math.floor((Math.random() * 400) + 120);
+            var building = draw.rect(75,buildingHeight + 14,'Brown','Red',1);
+            building.x = 200*i;
+            building.y = groundY-buildingHeight;
+            background.addChild(building);
+            buildings.push(building);
+}
+     
             // TODO 4: Part 1 - Add a tree
-            
-            
-        } // end of render function - DO NOT DELETE
+            tree = draw.bitmap('img/tree.png');
+                tree.x = 700;
+                tree.y = 345;
+                background.addChild(tree);
+            } 
+        // end of render function - DO NOT DELETE
         
         
         // Perform background animation
@@ -60,11 +86,20 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 4: Part 2 - Move the tree!
-            
-            
-            // TODO 5: Part 2 - Parallax
-            
+            tree.x = tree.x - 1;
 
+            if(tree.x < -200) {
+             tree.x = canvasWidth;
+            }
+            // TODO 5: Part 2 - Parallax
+           for(var i = 0; i < buildings.length; i++){ 
+                    var building = buildings[i]; 
+            
+                building.x = building.x - 1.7 
+            if(building.x < -200) {
+             building.x = canvasWidth;
+            }
+                }   
         } // end of update function - DO NOT DELETE
         
         
